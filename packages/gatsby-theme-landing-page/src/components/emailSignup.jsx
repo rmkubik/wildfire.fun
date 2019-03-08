d2433384d121496acb7d63c4716acfd5a23b3246
Ryan Kubik <rmkubik@me.com>
2019-03-07 22:27:54 -0800

    Create emailSignUp component

--- packages/gatsby-theme-landing-page/src/components/emailSignup.jsx
+++ packages/gatsby-theme-landing-page/src/components/emailSignup.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import emailSignUpStyles from './emailSignUp.module.scss';
 
-export default () => (
-  <form
-    action="https://buttondown.email/api/emails/embed-subscribe/rmkubik"
-    method="post"
-    target="popupwindow"
-    onSubmit={() =>
-      window.open('https://buttondown.email/rmkubik', 'popupwindow')
-    }
-    className="embeddable-buttondown-form"
-  >
-    <label htmlFor="bd-email" id="bd-email">
-      Enter your email
-      <input type="email" name="email" id="bd-email" />
-    </label>
-    <input type="hidden" value="1" name="embed" />
-    <input type="submit" value="Subscribe" />
-  </form>
+export default ({ label }) => (
+  <div className={emailSignUpStyles.container}>
+    <form
+      action="https://buttondown.email/api/emails/embed-subscribe/rmkubik"
+      method="post"
+      target="popupwindow"
+      onSubmit={() =>
+        window.open('https://buttondown.email/rmkubik', 'popupwindow')
+      }
+      className={emailSignUpStyles.form}
+    >
+      <label htmlFor="bd-email" id="bd-email">
+        <input
+          type="email"
+          name="email"
+          id="bd-email"
+          className={emailSignUpStyles.email}
+        />
+        <input
+          type="submit"
+          value="Subscribe"
+          className={emailSignUpStyles.submit}
+        />
+        <input type="hidden" value="1" name="embed" />
+        <span>{label}</span>
+      </label>
+    </form>
+  </div>
 );
