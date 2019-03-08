00f2fbeeb3afbf8bfbe34b76b6aa3ffffff74334
Ryan Kubik <ryan.kubik@nike.com>
2019-03-08 10:48:40 -0800

    Move theme and vars into theme directory

--- packages/gatsby-theme-landing-page/src/components/layout.jsx
+++ packages/gatsby-theme-landing-page/src/components/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import layoutStyles from './layout.module.scss';
-import './theme.scss';
+import '../theme/layout.scss';
 
 export default ({ children }) => (
   <div className={layoutStyles.column}>
