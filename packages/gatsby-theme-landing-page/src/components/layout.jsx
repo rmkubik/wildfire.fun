3ecb6969abba2445f1bd26bcb7d51a6317038038
Ryan Kubik <rmkubik@me.com>
2019-03-09 08:48:50 -0800

    Create Twitter meta data with Helmet

--- packages/gatsby-theme-landing-page/src/components/layout.jsx
+++ packages/gatsby-theme-landing-page/src/components/layout.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import layoutStyles from './layout.module.scss';
+import twitterTokens from '../tokens/twitter';
 import './theme.scss';
 
 export default ({ children }) => (
   <div className={layoutStyles.column}>
+    <Helmet>
+      <title>Wildfire</title>
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content={twitterTokens.handle} />
+      <meta
+        name="twitter:title"
+        content={twitterTokens.card.title}
+      />
+      <meta name="twitter:description" content={twitterTokens.card.description} />
+      <meta
+        name="twitter:image"
+        content={twitterTokens.card.image}
+      />
+    </Helmet>
     <main className={layoutStyles.layout}>{children}</main>
   </div>
 );
---
+++ packages/gatsby-theme-landing-page/src/tokens/twitter.js
@@ -0,0 +1,10 @@
+import worldMap from '../../assets/images/world-map.png';
+
+export default {
+  handle: '@ryrykubes',
+  card: {
+    title: 'Wildfire: Swap Tiles to Put Out Fires',
+    description: "Wildfire is a puzzle game where you swap tiles to contain raging forest fires before they destroy people's homes.",
+    image: worldMap
+  }
+}
