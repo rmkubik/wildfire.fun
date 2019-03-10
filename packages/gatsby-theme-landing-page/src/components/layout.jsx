dba941f87e554878b95639deafe3e362d1034718
Ryan Kubik <rmkubik@me.com>
2019-03-10 13:19:52 -0700

    Add OpenGraph meta tags

--- packages/gatsby-theme-landing-page/src/components/layout.jsx
+++ packages/gatsby-theme-landing-page/src/components/layout.jsx
@@ -18,6 +18,12 @@ export default ({ children }) => (
             { name: 'twitter:description', content: twitterTokens.card.description },
             { name: 'twitter:image:src', content: `${twitterTokens.siteUrl}${twitterTokens.card.image}` },
             { name: 'twitter:url', content: twitterTokens.siteUrl },
+            { name: 'og:title', content: twitterTokens.card.title },
+            { name: 'og:site_name', content: twitterTokens.card.title },
+            { name: 'og:url', content: twitterTokens.siteUrl },
+            { name: 'og:description', content: twitterTokens.card.description },
+            { name: 'og:image', content: `${twitterTokens.siteUrl}${twitterTokens.card.image}` },
+            { name: 'og:type', content: 'website' },
           ]}
           title={twitterTokens.card.title}
         />
