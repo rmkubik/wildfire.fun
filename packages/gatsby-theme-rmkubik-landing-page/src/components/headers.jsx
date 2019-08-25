fb502546d7482d63cd1bdf9028bf4433534abdde
Ryan Kubik <rmkubik@me.com>
2019-08-24 17:21:44 -0700

    Add header links

--- packages/gatsby-theme-rmkubik-landing-page/src/components/headers.jsx
+++ packages/gatsby-theme-rmkubik-landing-page/src/components/headers.jsx
@@ -1,8 +1,10 @@
-import kebabCase from 'kebab-case';
+import dashify from 'dashify';
 import React from 'react';
 
 // const H1 = props => <Header is="h1" {...props} />;
-const H2 = ({ title }) => <h2 id={kebabCase(title).slice(1)}>{title}</h2>;
+const H2 = ({ title }) => (
+  <h2 id={dashify(title, { condense: true })}>{title}</h2>
+);
 // const H3 = props => <Header is="h3" {...props} />;
 // const H4 = props => <Header is="h4" {...props} />;
 // const H5 = props => <Header is="h5" {...props} />;
