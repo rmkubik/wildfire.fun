a84830f089b867b909a66bdbb7fe9b7a44f7ec03
Ryan Kubik <rmkubik@me.com>
2019-08-25 21:59:34 -0700

    Finish adding stills to presskit

--- packages/gatsby-theme-rmkubik-landing-page/src/components/images.jsx
+++ packages/gatsby-theme-rmkubik-landing-page/src/components/images.jsx
@@ -7,8 +7,13 @@ import windyRoundaboutGif from '../../assets/images/windy-roundabout-fail.2019-0
 import tallGrassGif from '../../assets/images/wildfire-locked-tile.gif';
 import titleWithByline from '../../assets/images/wildfire-title-with-byline.png';
 import title from '../../assets/images/titleAndLogo.png';
+import onlyYouCanPlay from '../../assets/images/only-you-can-play.png';
+import world1 from '../../assets/images/world1-still.png';
+import world2 from '../../assets/images/tall-grass-ash-world2.png';
+import world3 from '../../assets/images/image-windy-world3.png';
 
 const images = {
+  onlyYouCanPlay,
   trickyGif,
   fireEmoji,
   titleScreenGif,
@@ -17,6 +22,9 @@ const images = {
   tallGrassGif,
   titleWithByline,
   title,
+  world1,
+  world2,
+  world3,
 };
 
 const Image = ({ src, alt }) => <img src={images[src]} alt={alt} />;
