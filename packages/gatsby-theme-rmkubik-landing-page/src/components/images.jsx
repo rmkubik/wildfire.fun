eda8da97ccf8889d1afd3a310492a4f6c7efbb84
Ryan Kubik <rmkubik@me.com>
2019-08-25 21:04:44 -0700

    Add logo icons and gifs

--- packages/gatsby-theme-rmkubik-landing-page/src/components/images.jsx
+++ packages/gatsby-theme-rmkubik-landing-page/src/components/images.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import fireEmoji from '../../assets/images/fire-emoji.png';
+import fireEmoji from '../../assets/images/wildfire-fire_magnify.png';
 import titleScreenGif from '../../assets/images/wildire-worldmap-idle.gif';
+import trickyGif from '../../assets/images/wildfire-success-modal.gif';
+import world1LevelNamesGif from '../../assets/images/wildfire-worldmap.gif';
+import windyRoundaboutGif from '../../assets/images/windy-roundabout-fail.2019-08-19 09_56_12.gif';
+import tallGrassGif from '../../assets/images/wildfire-locked-tile.gif';
+import titleWithByline from '../../assets/images/wildfire-title-with-byline.png';
+import title from '../../assets/images/titleAndLogo.png';
 
 const images = {
+  trickyGif,
   fireEmoji,
   titleScreenGif,
+  world1LevelNamesGif,
+  windyRoundaboutGif,
+  tallGrassGif,
+  titleWithByline,
+  title,
 };
 
 const Image = ({ src, alt }) => <img src={images[src]} alt={alt} />;
