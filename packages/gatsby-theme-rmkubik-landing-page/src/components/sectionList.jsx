7b5879081b08645ce99b672c5534caafc17964ca
Ryan Kubik <rmkubik@me.com>
2019-08-26 22:11:23 -0700

    Add image to section list of presskit

--- packages/gatsby-theme-rmkubik-landing-page/src/components/sectionList.jsx
+++ packages/gatsby-theme-rmkubik-landing-page/src/components/sectionList.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import sectionListStyles from './sectionList.module.scss';
+import Image from './images';
 
 const SectionList = ({ links }) => {
   return (
-    <div className={sectionListStyles.header}>
-      {links.map(({ label, link }) => (
-        <a href={link}>{label}</a>
-      ))}
+    <div style={{ display: 'flex' }}>
+      <div style={{ display: 'block', flexShrink: '0', marginRight: '3rem' }}>
+        <div className={sectionListStyles.header}>
+          {links.map(({ label, link }) => (
+            <a href={link}>{label}</a>
+          ))}
+        </div>
+      </div>
+      <div>
+        <Image src="titleScreenGif" alt="Wildfire title screen" />
+      </div>
     </div>
   );
 };
