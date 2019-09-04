211f948a7930e69875c96d7ae92641b0c71c2d35
Ryan Kubik <rmkubik@me.com>
2019-09-03 23:07:37 -0700

    Update email component to feed to mailchimp

--- packages/gatsby-theme-rmkubik-landing-page/src/components/emailSignup.jsx
+++ packages/gatsby-theme-rmkubik-landing-page/src/components/emailSignup.jsx
@@ -2,6 +2,66 @@ import React from 'react';
 import emailSignUpStyles from './emailSignUp.module.scss';
 
 export default ({ label }) => (
+  <div id="mc_embed_signup" className={emailSignUpStyles.container}>
+    <form
+      action="https://ryankubik.us17.list-manage.com/subscribe/post?u=9b8c018898653292cbe3711f0&amp;id=57cdfbb895"
+      method="post"
+      id="mc-embedded-subscribe-form"
+      name="mc-embedded-subscribe-form"
+      className={`validate ${emailSignUpStyles.form}`}
+      target="_blank"
+      noValidate
+    >
+      <div id="mc_embed_signup_scroll">
+        <div className="mc-field-group">
+          <label htmlFor="mce-EMAIL">
+            <input
+              type="email"
+              name="EMAIL"
+              placeholder="email"
+              className={`required email ${emailSignUpStyles.email}`}
+              id="mce-EMAIL"
+            />
+            <input
+              type="submit"
+              value="Subscribe"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              className={`button ${emailSignUpStyles.submit}`}
+            />
+            <span>{label}</span>
+          </label>
+        </div>
+        <div id="mce-responses" className="clear">
+          <div
+            className="response"
+            id="mce-error-response"
+            style={{ display: 'none' }}
+          />
+          <div
+            className="response"
+            id="mce-success-response"
+            style={{ display: 'none' }}
+          />
+        </div>
+        <div
+          style={{ position: 'absolute', left: '-5000px' }}
+          aria-hidden="true"
+        >
+          <input
+            type="text"
+            name="b_9b8c018898653292cbe3711f0_57cdfbb895"
+            tabIndex="-1"
+            value=""
+          />
+        </div>
+      </div>
+    </form>
+  </div>
+);
+
+/*
+export default ({ label }) => (
   <div className={emailSignUpStyles.container}>
     <form
       action="https://buttondown.email/api/emails/embed-subscribe/rmkubik"
@@ -31,3 +91,4 @@ export default ({ label }) => (
     </form>
   </div>
 );
+*/
